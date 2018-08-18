a50f1e974fa5b3eca3e81f32dcae61c5e2c97dc1
JimYeh <yamap52088@gmail.com>
2018-08-18 16:39:49 +0800

    ✨ Feat: Add Speak

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,6 +11,33 @@ export default {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+
+  mounted () {
+    this.speakInit()
+  },
+
+  methods: {
+    speakInit () {
+      const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+      const recognition = new SpeechRecognition()
+
+      recognition.lang = 'zh-TW'
+      recognition.interimResults = false
+
+      recognition.addEventListener('result', (e) => {
+        let last = e.results.length - 1
+        let text = e.results[last][0].transcript
+
+        console.log(text)
+      })
+
+      recognition.addEventListener('end', () => {
+        recognition.start()
+      })
+
+      recognition.start()
+    }
   }
 }
 </script>
